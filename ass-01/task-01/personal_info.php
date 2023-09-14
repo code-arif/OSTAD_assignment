3ae6bb34b8cbd207461918bfb0361887bd6c36f9
code-arif <arifulislam6460@gmail.com>
2023-09-14 15:16:57 +0600

    simply personal information assign to varible and print with echo

---
+++ ass-01/task-01/personal_info.php
@@ -0,0 +1,9 @@
+<?php 
+//personal information
+$name = "Md. Ariful Islam";
+$age = 22;
+$country = "Bangladesh";
+$intro = "I aspire to become a professional in PHP and Laravel. '</br>'These two topics drive my passion for web development, '</br>' and I'm eager to master them for a successful career.";
+
+echo "My name is {$name}. I'm {$age} years old. {$intro}";
+?>
